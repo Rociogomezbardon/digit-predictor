e19f60733dec3b5f09bb5596e3ee5d233291bf85
Rocio Gomez <rociogomezbardon@gmail.com>
2020-09-18 18:22:34 +0300

    cleaning code

--- digit_predictor/synthetic_digits.py
+++ digit_predictor/synthetic_digits.py
@@ -2,6 +2,22 @@
 This application creates images with non-zero digits (1 - 9) in black background and white font.The images are (imsize,imsize) size, and they are converted into (1,imsize*imsize) size, and all of them are saved in 'digits.data' and their true values are saved in 'labels.data', one entry per line. They can be read with np.loadtxt('digits.data') which will return an array of images.
 Use  images = np.loadtxt('digits.data').reshape([-1,imsize,imsize]) to reshape images before using them.
 '''
+'''
+This is the list of fonts used by cv2. We chose a few of them to create our synthetic dataset.
+
+FONT_HERSHEY_SIMPLEX        = 0, //!< normal size sans-serif font
+FONT_HERSHEY_PLAIN          = 1, //!< small size sans-serif font
+FONT_HERSHEY_DUPLEX         = 2, //!< normal size sans-serif font (more complex
+    than FONT_HERSHEY_SIMPLEX)
+FONT_HERSHEY_COMPLEX        = 3, //!< normal size serif font
+FONT_HERSHEY_TRIPLEX        = 4, //!< normal size serif font (more complex than
+    FONT_HERSHEY_COMPLEX)
+FONT_HERSHEY_COMPLEX_SMALL  = 5, //!< smaller version of FONT_HERSHEY_COMPLEX
+FONT_HERSHEY_SCRIPT_SIMPLEX = 6, //!< hand-writing style font
+FONT_HERSHEY_SCRIPT_COMPLEX = 7, //!< more complex variant of
+    FONT_HERSHEY_SCRIPT_SIMPLEX
+FONT_ITALIC                 = 16 //!< flag for italic font
+'''
 
 import cv2
 import numpy as np
@@ -28,17 +44,11 @@ def get_digit_pil_font(digit,font_path):
     font = ImageFont.truetype(font_path, 90)
     draw.text((0, 0), digit, font=font, fill=255)
     cv_img = np.array(img)
-    '''
-    if 'adf/AccanthisADFStdNo2-Italic.otf' in font_path:
-        cv2.imshow('img', cv_img)
-        cv2.waitKey(0)
-    '''
     contours, _ = cv2.findContours(cv_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     [x,y,w,h] = cv2.boundingRect(contours[0])
     cv_img = cv_img[y:y+h,x:x+w]
     cv_img = cv2.resize(cv_img,(int(h*2/3),h))
     return cv_img
-
 
 def get_digit_cv_font(digit,font, imsize, lineType):
     #the digits written at scale 1 cover around 17x22 pixels
@@ -52,18 +62,6 @@ def get_digit_cv_font(digit,font, imsize, lineType):
                 font,fontScale,fontColor,lineType)
     return cv_img
 
-def create_images_and_labels(images_file, labels_file, imsize):
-    cv_digits_array,cv_labels_array = get_cv_font_images(imsize)
-    pil_digits_array,pil_labels_array = get_pil_font_images(imsize)
-
-    img_digits_array =  np.vstack((cv_digits_array , pil_digits_array))
-
-    labels_array = cv_labels_array + pil_labels_array
-    if(images_file):
-        np.savetxt(images_file, img_digits_array)
-    if(labels_file):
-        np.savetxt(labels_file, np.array(labels_array), fmt='%i')
-
 def get_cv_font_images(imsize):
     img_digits_array = None
     labels_array = []
@@ -76,10 +74,6 @@ def get_cv_font_images(imsize):
                 labels_array.append(digit)
     return img_digits_array, labels_array
 
-
-
-
-
 def get_pil_font_images(imsize):
     img_digits_array = None
     labels_array = []
@@ -91,39 +85,19 @@ def get_pil_font_images(imsize):
 
     for digit in range(1,10):
         for path in fonts_paths:
-            #print(path)
             img = get_digit_pil_font(str(digit),path)
-            #if 'adf/AccanthisADFStdNo2-Italic.otf' in path:
-            #    print(path)
-            #    cv2.imshow('img', img)
-            #    cv2.waitKey(0)
             img = pad_and_resize(img,imsize)
-            #cv2.imshow('img', img)
-            #cv2.waitKey(0)
             if img_digits_array is None: img_digits_array = img.reshape([1, imsize**2])
             else:img_digits_array = np.vstack((img_digits_array, img.reshape([1, imsize**2])))
             labels_array.append(digit)
     return img_digits_array, labels_array
 
-if __name__ == "__main__":
-    imsize = 56
-    images_file = 'data/DIGITS/digits.data'
-    labels_file = 'data/DIGITS/labels.data'
-    create_images_and_labels(images_file, labels_file, imsize )
-
-'''
-This is the list of fonts used by cv2. We chose a few of them to create our synthetic dataset.
-
-FONT_HERSHEY_SIMPLEX        = 0, //!< normal size sans-serif font
-FONT_HERSHEY_PLAIN          = 1, //!< small size sans-serif font
-FONT_HERSHEY_DUPLEX         = 2, //!< normal size sans-serif font (more complex
-    than FONT_HERSHEY_SIMPLEX)
-FONT_HERSHEY_COMPLEX        = 3, //!< normal size serif font
-FONT_HERSHEY_TRIPLEX        = 4, //!< normal size serif font (more complex than
-    FONT_HERSHEY_COMPLEX)
-FONT_HERSHEY_COMPLEX_SMALL  = 5, //!< smaller version of FONT_HERSHEY_COMPLEX
-FONT_HERSHEY_SCRIPT_SIMPLEX = 6, //!< hand-writing style font
-FONT_HERSHEY_SCRIPT_COMPLEX = 7, //!< more complex variant of
-    FONT_HERSHEY_SCRIPT_SIMPLEX
-FONT_ITALIC                 = 16 //!< flag for italic font
-'''
+def create_images_and_labels(images_file, labels_file, imsize):
+    cv_digits_array,cv_labels_array = get_cv_font_images(imsize)
+    pil_digits_array,pil_labels_array = get_pil_font_images(imsize)
+    img_digits_array =  np.vstack((cv_digits_array , pil_digits_array))
+    labels_array = cv_labels_array + pil_labels_array
+    if(images_file):
+        np.savetxt(images_file, img_digits_array)
+    if(labels_file):
+        np.savetxt(labels_file, np.array(labels_array), fmt='%i')
--- digit_predictor/train/train.py
+++ digit_predictor/train/train.py
@@ -1,5 +1,4 @@
 import torch
-
 def train(model, train_loader,  epochs, device, learning_rate, momentum, best_model_path, first_epoch=1):
 
     loss_file = 'train/loss.csv'
@@ -7,35 +6,31 @@ def train(model, train_loader,  epochs, device, learning_rate, momentum, best_mo
         file = open(loss_file,'w+')
         file.write('epoch,loss\n')
         file.close()
-    
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum)
     loss_fn = torch.nn.CrossEntropyLoss()
     best_loss = 100
-    
+
     for epoch in range(first_epoch, epochs + 1):
-        
         model.to(device)
         model.train()
-
         total_loss = 0
+
         for batch in train_loader:
             batch_X, batch_y = batch
-
             batch_X = batch_X.to(device)
             batch_y = batch_y.to(device)
-
             model.zero_grad()
             output = model(batch_X)
-            loss = loss_fn(output, batch_y)  
+            loss = loss_fn(output, batch_y)
             loss.backward()
             optimizer.step()
             total_loss += loss.data.item()
-            
+
         file = open(loss_file,'a+')
         file.write(str(epoch)+','+str(total_loss)+'\n')
-        file.close()    
+        file.close()
         print("Epoch: {}, CELoss: {:f}".format(epoch, total_loss /len(train_loader)))
+
         if(total_loss < best_loss):
             best_loss = total_loss
             torch.save(model.state_dict(), best_model_path)
-        
