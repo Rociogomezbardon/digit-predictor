12bf5431d25f8dc1ed426172332e52f03b9d2cff
Rocio Gomez <rociogomezbardon@gmail.com>
2020-09-18 23:04:42 +0300

    clean code and restructure folders

---
+++ predictor.py
@@ -0,0 +1,30 @@
+import torch
+from digit_predictor.train import model
+import cv2
+import numpy as np
+
+
+global predictor
+
+def load_model(modelpath = 'digit_predictor/best_model.pth'):
+    global predictor
+    predictor = model.CNNet()
+    predictor.load_state_dict(torch.load(modelpath, map_location='cpu'))
+    for parameter in predictor.parameters():
+        parameter.requres_grad = False
+    predictor.eval()
+    return
+
+def getIMSIZE():
+    return model.IMSIZE
+
+def predict(imgs):
+    #if my_model is None: load_model()
+    imgs = np.expand_dims(imgs,1)    
+    if(np.max(imgs) > 1): imgs = imgs/255
+    tensor_imgs = torch.tensor(np.array(imgs)).float()
+    output = predictor(tensor_imgs).detach().numpy()
+    prediction = [np.argmax(np.squeeze(o))+1 for o in output]
+    return(prediction)
+
+
